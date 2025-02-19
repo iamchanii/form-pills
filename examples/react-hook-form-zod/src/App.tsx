1a72e971aa54b192bfb6022b95c7b2a278cac1e9
Ette (에테) <ette@daangn.com>
2025-02-19 10:21:32 +0900

    Fix

--- examples/react-hook-form-zod/src/App.tsx
+++ examples/react-hook-form-zod/src/App.tsx
@@ -5,41 +5,41 @@ import { Button } from './components/ui/button';
 import { AddressField, EmailField, UsernameField } from './fields';
 
 const formSchema = z.object({
-  ...UsernameField.schemaShape,
-  ...EmailField.schemaShape,
-  ...AddressField.schemaShape,
+	...UsernameField.schemaShape,
+	...EmailField.schemaShape,
+	...AddressField.schemaShape,
 });
 
 type FormSchemaType = z.infer<typeof formSchema>;
 
 export function App() {
-  const form = useForm<FormSchemaType>({
-    resolver: standardSchemaResolver(formSchema),
-    mode: 'onChange',
-    defaultValues: {
-      ...UsernameField.getDefaultValues(),
-      ...EmailField.getDefaultValues(),
-      ...AddressField.getDefaultValues(),
-    },
-  });
+	const form = useForm<FormSchemaType>({
+		resolver: standardSchemaResolver(formSchema),
+		mode: 'onChange',
+		defaultValues: {
+			...UsernameField.getDefaultValues(),
+			...EmailField.getDefaultValues(),
+			...AddressField.getDefaultValues(),
+		},
+	});
 
-  const onSubmit = form.handleSubmit((values) => {
-    console.log(values);
-  });
+	const onSubmit = form.handleSubmit((values) => {
+		console.log(values);
+	});
 
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <FormProvider {...form}>
-        <form onSubmit={onSubmit} className="space-y-4 w-90">
-          <UsernameField label="Username" />
-          <EmailField />
-          <AddressField />
+	return (
+		<div className="flex justify-center items-center h-screen">
+			<FormProvider {...form}>
+				<form onSubmit={onSubmit} className="space-y-4 w-90">
+					<UsernameField label="Username" />
+					<EmailField />
+					<AddressField />
 
-          <Button type="submit" size="lg" className="w-full">
-            Submit
-          </Button>
-        </form>
-      </FormProvider>
-    </div>
-  );
+					<Button type="submit" size="lg" className="w-full">
+						Submit
+					</Button>
+				</form>
+			</FormProvider>
+		</div>
+	);
 }
